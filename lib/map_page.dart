6abdb2f7920b8c20119d6178a54dadff33eac9a9
RYOTA2528 <1216022@gmail.com>
2022-11-01 23:22:05 +0900

    GoogleMapの表示完了

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -9,12 +9,22 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  late GoogleMapController _controller; //GoogleMapController controllerを格納するための変数
+  //初期値(最初の座標)
+  static const CameraPosition _initialPosition = CameraPosition(
+      target: LatLng(35.32504120390358, 139.55600562500453),
+      zoom: 16
+  );
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("GoogleMapの表示"),
-     )
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: _initialPosition, //初期値の座標
+        onMapCreated: (GoogleMapController controller) {
+          _controller = controller; //Mapが作成されたタイミングでGoogleMapController controllerを入れMapの制御を行えるようにする。
+        },
+      )
     );
   }
 }
