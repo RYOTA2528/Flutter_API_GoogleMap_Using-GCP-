07d045fc7c15c1d7bb623e7950d38048fdb9eed5
RYOTA2528 <1216022@gmail.com>
2022-11-03 16:58:44 +0900

    現在地を取得しマーカーで表示

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
@@ -24,14 +25,40 @@ class _MapPageState extends State<MapPage> {
     )
   };
 
+  late final CameraPosition currentPosition;
+  //現在地を取得するメソッド
+  Future<void> getCurrentPosition() async {
+    //現在地を取得する許可があるかの確認
+    LocationPermission permission = await Geolocator.checkPermission();
+    if(permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission(); //まだ権限が付与されてない分岐のため付与のrequest
+      if(permission == LocationPermission.denied) {
+        Future.error('現在地を取得は出来ません。'); //requestを送ってもエラーならerrorメッセージを表示
+      }
+      final Position _currentPosition = await Geolocator.getCurrentPosition(); //現在地を取得
+      currentPosition = CameraPosition(target: LatLng(_currentPosition.latitude, _currentPosition.longitude), zoom: 16); //カメラの位置を記載
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
         markers: _markers,
         initialCameraPosition: _initialPosition, //初期値の座標
-        onMapCreated: (GoogleMapController controller) {
+        onMapCreated: (GoogleMapController controller) async{
+          await getCurrentPosition(); //カメラに位置情報を取得させる（メソッド内で現在地取得にしてる）
           _controller = controller; //Mapが作成されたタイミングでGoogleMapController controllerを入れMapの制御を行えるようにする。
+          setState(() {
+            _markers.add(Marker(
+                markerId: const MarkerId('2'),
+                position: currentPosition.target,
+                infoWindow: const InfoWindow(title: '現在地')
+            ));
+          });
+          //実際にanimateCameraメソッドでカメラの位置を現在地に動かすメソッドを記載。
+          _controller.animateCamera(CameraUpdate.newCameraPosition(currentPosition));
         },
       )
     );
