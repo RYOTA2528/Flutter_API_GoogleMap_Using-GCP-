aed58f8842cd133a3b717354eb3830bc7d3430ba
RYOTA2528 <1216022@gmail.com>
2022-11-05 11:36:34 +0900

    検索機能の追加

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -25,6 +26,12 @@ class _MapPageState extends State<MapPage> {
     )
   };
 
+  //住所検索用のメソッド
+  Future<CameraPosition> searchLatLng(String address) async {
+    List<Location> locations = await locationFromAddress(address); //入力された住所から緯度・経度ど取得できる
+    return CameraPosition(target: LatLng(locations[0].latitude, locations[0].longitude), zoom: 16); //とってきた値の緯度・経度を格納したものを返り値
+  }
+
   late final CameraPosition currentPosition;
   //現在地を取得するメソッド
   Future<void> getCurrentPosition() async {
@@ -44,22 +51,44 @@ class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        markers: _markers,
-        initialCameraPosition: _initialPosition, //初期値の座標
-        onMapCreated: (GoogleMapController controller) async{
-          await getCurrentPosition(); //カメラに位置情報を取得させる（メソッド内で現在地取得にしてる）
-          _controller = controller; //Mapが作成されたタイミングでGoogleMapController controllerを入れMapの制御を行えるようにする。
-          setState(() {
-            _markers.add(Marker(
-                markerId: const MarkerId('2'),
-                position: currentPosition.target,
-                infoWindow: const InfoWindow(title: '現在地')
-            ));
-          });
-          //実際にanimateCameraメソッドでカメラの位置を現在地に動かすメソッドを記載。
-          _controller.animateCamera(CameraUpdate.newCameraPosition(currentPosition));
-        },
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 4.0),
+        child: SafeArea(
+          child: Column(
+            children: [
+                 TextField(
+                   decoration: const InputDecoration(
+                     border: OutlineInputBorder()
+                   ),
+                   onSubmitted: (value) async{ //(value)にはこのボタンを押した際TextFiledで入力された情報が入ってくる。
+                     CameraPosition result = await searchLatLng(value); //value（住所）から取得したカメラポジションをresult変数へ格納
+                     _controller.animateCamera(CameraUpdate.newCameraPosition(result)); //これで実際に住所の位置へカメラを移動
+                   },
+                 ),
+              Expanded(
+                child: GoogleMap(
+                  markers: _markers,
+                  initialCameraPosition: _initialPosition, //初期値の座標
+                  onMapCreated: (GoogleMapController controller) async{
+                    await getCurrentPosition(); //カメラに位置情報を取得させる（メソッド内で現在地取得にしてる）
+                    _controller = controller; //Mapが作成されたタイミングでGoogleMapController controllerを入れMapの制御を行えるようにする。
+                    setState(() {
+                      _markers.add(Marker(
+                          markerId: const MarkerId('2'),
+                          position: currentPosition.target,
+                          infoWindow: const InfoWindow(title: '現在地')
+                      ));
+                    });
+                    //実際にanimateCameraメソッドでカメラの位置を現在地に動かすメソッドを記載。
+                    _controller.animateCamera(CameraUpdate.newCameraPosition(currentPosition));
+                  },
+                  myLocationEnabled: true,
+                  myLocationButtonEnabled: true,
+                ),
+              ),
+            ],
+          ),
+        ),
       )
     );
   }
